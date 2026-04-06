d08fd9b3223f8cc5cf2cc4839d42787d1c76d594
jookettle <wnwlsvy0914@gmail.com>
2026-04-06 23:16:11 +0900

    feat: initialize base HTML template and root layout with global styles and fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,12 @@
 </script>
 
 <svelte:head>
-	<title>세벌식 타자연습</title>
-	<meta name="description" content="Master the Se-yeonsik 3-set keyboard layout with our premium typing practice application." />
+	<title>세연식 세벌식 타자연습</title>
+	<meta name="description" content="세벌식의 매력을 더 깊게, 세련된 인터페이스로 즐기는 타자 연습 서비스" />
+	<meta property="og:title" content="세벌식 타자연습" />
+	<meta property="og:description" content="세벌식의 매력을 더 깊게, 세련된 인터페이스로 즐기는 타자 연습 서비스" />
+	<meta property="og:type" content="website" />
+	<meta name="twitter:card" content="summary_large_image" />
 	<link rel="icon" type="image/png" href="/favicon.png" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
